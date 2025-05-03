c6ad37d962d0162ac90c4f2f24ffd93ca72cff7d
jitesh <jitesh02sep@gmail.com>
2025-05-03 23:48:21 +0530

    adding the router for category

--- backend/routes/CategoryRoute.js
+++ backend/routes/CategoryRoute.js
@@ -1,2 +1,22 @@
-const express = require('express');
+import express from "express";
+import {
+  createCategory,
+  getAllCategories,
+  getCategoryById,
+  updateCategory,
+  deleteCategory,
+} from "../controllers/CategoryController.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
+const router = express.Router();
+
+// Public
+router.get("/", getAllCategories);
+router.get("/:id", getCategoryById);
+
+// Protected
+router.post("/", requireSignIn, isAdmin, createCategory);
+router.put("/:id", requireSignIn, isAdmin, updateCategory);
+router.delete("/:id", requireSignIn, isAdmin, deleteCategory);
+
+export default router;
