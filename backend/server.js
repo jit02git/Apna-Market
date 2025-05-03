73ccbb86841e683d4c6784e8ba575c7e76a5be59
jitesh <jitesh02sep@gmail.com>
2025-05-03 23:50:15 +0530

    linked category with the server

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/AuthRoute');
+const categoryRoutes = require('./routes/CategoryRoute');
 const productRoutes = require('./routes/ProductRoute');
 const orderRoutes = require('./routes/OrderRoute');
 const cartRoutes = require('./routes/CartRoute');
-const cors = require('cors');
+const cors = require('cors')
 const app = express();
 
 dotenv.config();
@@ -19,6 +20,8 @@ app.use('/api', authRoutes);
 app.use('/api/products', productRoutes);
 app.use('/api/order', orderRoutes);
 app.use('/api/cart', cartRoutes);
+app.use("/api/categories", categoryRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
